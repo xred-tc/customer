2e1b8522c4079fdb66f3958ae1372c5fd1068fc5
xred-tc <124847939+xred-tc@users.noreply.github.com>
2023-02-14 16:15:53 +0800

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,14 @@
+<template>
+    <div>
+    <button class="btn btn-danger">Button</button>
+    <br>
+    <NuxtLink to="/testcss">這是修改後的模板</NuxtLink>
+    <br>
+    <NuxtLink to="/origin">這是原始模板</NuxtLink>
+
+    <!-- <br>
+    <NuxtLink to="/table">這是表格</NuxtLink> -->
+  </div>
+</template>
+
 
---
+++ pages/testcss.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <Sidebar v-bind:isselected ="isselected"></Sidebar>
+        <Content @getThisId="getThisId"></Content>
+    </div>
+<!-- End of Page Wrapper -->
+
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            isselected:1
+        } 
+    },
+    methods: {
+        getThisId(value) {
+            this.isselected = value;
+        }
+    }
+}
+
+</script>
